9e95e53d8b3e6c06dda647efc068ae9db2f0db57
Rafael <rapha.pse@outlook.com>
2017-03-16 21:01:38 -0300

    Correções

--- workspace/teste/src/com/rcc/factory/ConexaoFactory.java
+++ workspace/teste/src/com/rcc/factory/ConexaoFactory.java
@@ -1,7 +1,30 @@
 package com.rcc.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoFactory {
 	
+	private static final String USUARIO = "root";
+	private static final String SENHA = "q1w2e3r4";
+	private static final String URL = "jdbc:mysql://localhost:3306/drogaria";
 	
+	public static Connection conectar() throws SQLException{
+		
+		Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return connection;
+		
+	}
+	public static void main(String[] args) {
+		try {
+			Connection connection = ConexaoFactory.conectar();
+			System.out.println("Certo");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Errado");
+		}
+	}
 
 }
