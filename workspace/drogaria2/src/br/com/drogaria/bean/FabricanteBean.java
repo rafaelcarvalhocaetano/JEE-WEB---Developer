764e2ee8de81f6e0b92af6fa6e4752c8748f3fbb
Rafael <rapha.pse@outlook.com>
2017-03-19 15:18:21 -0300

    Criando layout através do template e também menu

---
+++ workspace/drogaria2/src/br/com/drogaria/bean/FabricanteBean.java
@@ -0,0 +1,42 @@
+package br.com.drogaria.bean;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.ListDataModel;
+
+import br.com.drogaria.dao.FabricanteDAO;
+import br.com.drogaria.domain.Fabricante;
+
+@ManagedBean(name = "MBFabricante")
+@ViewScoped
+public class FabricanteBean {
+
+	private ListDataModel<Fabricante> itens;
+
+	public ListDataModel<Fabricante> getItens() {
+		return itens;
+	}
+
+	public void setItens(ListDataModel<Fabricante> itens) {
+		this.itens = itens;
+	}
+
+	@PostConstruct
+	public void prepararPesquisa() {
+
+		try {
+			
+			FabricanteDAO dao = new FabricanteDAO();
+			ArrayList<Fabricante> lista = dao.listar();
+			itens = new ListDataModel<Fabricante>(lista);
+			
+		} catch (SQLException e) {
+			System.out.println("Erro no precessamento");
+			e.printStackTrace();
+		}
+	}
+}
