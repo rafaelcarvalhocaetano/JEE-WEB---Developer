45c8e88b91651cfb3eda192eb46f07cc91bb7bdf
Rafael <rapha.pse@outlook.com>
2017-03-23 20:21:01 -0300

    Criando DAO do produto e testando com JUnit

--- workspace/drogaria2/src/br/com/drogaria/Test/ProdutoDAOTeste.java
+++ workspace/drogaria2/src/br/com/drogaria/Test/ProdutoDAOTeste.java
@@ -1,5 +1,19 @@
 package br.com.drogaria.Test;
 
+import br.com.drogaria.domain.Fabricante;
+import br.com.drogaria.domain.Produto;
+
 public class ProdutoDAOTeste {
+	
+	public void salvar(){
+		Produto p = new Produto();
+		p.setDescricao("Novalgina com 10 comprimidos");
+		p.setPreco(2.45);
+		p.setQuantidade(13L);
+		
+		Fabricante f = new Fabricante();
+		f.setCodigo(18L);
+		
+	}
 
 }
