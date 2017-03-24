56d2f1a65faffaaf29e6c36d486897d0622fc295
Rafael <rapha.pse@outlook.com>
2017-03-24 08:51:18 -0300

    Criando camadas DAO do produto

--- workspace/drogaria2/src/br/com/drogaria/dao/ProdutoDAO.java
+++ workspace/drogaria2/src/br/com/drogaria/dao/ProdutoDAO.java
@@ -2,8 +2,11 @@ package br.com.drogaria.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.domain.Produto;
 import br.com.drogaria.factory.ConexaoFactory;
 
@@ -27,5 +30,41 @@ public class ProdutoDAO {
 		ppt.setLong(4, p.getFabricante().getCodigo());
 		
 		ppt.executeUpdate();
+	}
+	
+	public ArrayList<Produto> lista()throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		
+		sql.append("SELECT p.codigo, p.descricao, p.preco, p.quantidade, f.codigo, f.descricao "); 
+		sql.append("FROM produto p ");
+		sql.append("INNER JOIN fabricante f ON f.codigo = p.fabricante_codigo ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement ppt = conexao.prepareStatement(sql.toString());
+		
+		ResultSet resultado = ppt.executeQuery();
+		
+		ArrayList<Produto> itens = new ArrayList<Produto>();
+		
+		while(resultado.next()){
+			Fabricante f = new Fabricante();
+			f.setCodigo(resultado.getLong("f.codigo"));
+			f.setDescricao(resultado.getString("f.descricao"));
+			
+			
+			Produto p = new Produto();
+			p.setCodigo(resultado.getLong("p.codigo"));
+			p.setDescricao(resultado.getString("p.descricao"));
+			p.setPreco(resultado.getDouble("p.preco"));
+			p.setQuantidade(resultado.getLong("p.quantidade"));
+			
+			p.setFabricante(f);
+			
+			itens.add(p);
+		}
+		
+		return itens;
+		
 	}
 }
