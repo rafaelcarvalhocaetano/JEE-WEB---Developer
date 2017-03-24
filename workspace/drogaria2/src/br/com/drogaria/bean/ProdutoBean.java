8c85c3b271c4d355cfe4e99a29ba6b025f5a6f78
Rafael <rapha.pse@outlook.com>
2017-03-24 17:06:16 -0300

    Criando integração com o xhtml e o manegebean - produto

--- workspace/drogaria2/src/br/com/drogaria/bean/ProdutoBean.java
+++ workspace/drogaria2/src/br/com/drogaria/bean/ProdutoBean.java
@@ -3,45 +3,67 @@ package br.com.drogaria.bean;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.drogaria.dao.ProdutoDAO;
+import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.domain.Produto;
 import br.com.drogaria.util.JSFUtil;
 
-@ManagedBean(name="MBProduto")
+@ManagedBean(name = "MBProduto")
 @ViewScoped
 public class ProdutoBean {
 
 	private ArrayList<Produto> itens;
+	private ArrayList<Produto> itensFiltrados;
+	private Produto produto;
+	private ArrayList<Fabricante> comboFabricante;
 	
-	
-	
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public ArrayList<Fabricante> getComboFabricante() {
+		return comboFabricante;
+	}
+
+	public void setComboFabricante(ArrayList<Fabricante> comboFabricante) {
+		this.comboFabricante = comboFabricante;
+	}
+
+	public ArrayList<Produto> getItensFiltrados() {
+		return itensFiltrados;
+	}
+
+	public void setItensFiltrados(ArrayList<Produto> itensFiltrados) {
+		this.itensFiltrados = itensFiltrados;
+	}
+
 	public ArrayList<Produto> getItens() {
 		return itens;
 	}
-
-
 
 	public void setItens(ArrayList<Produto> itens) {
 		this.itens = itens;
 	}
 
+	public void carregarListagem() {
 
-
-	public void carregarListagem(){
-		
 		try {
-			
+
 			ProdutoDAO dao = new ProdutoDAO();
 			itens = dao.lista();
-						
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			JSFUtil.adicionarMensagemErro(e.getMessage());
 		}
-		
+
 	}
 }
